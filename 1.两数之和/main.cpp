5bb0a19127b42c652eb8e0f37f1f59aad2826014
Marcusxixi <1654400916@qq.com>
2025-03-03 14:08:41 +0800

    hash_map

---
+++ 1.两数之和/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+
+class Solution {
+    public: vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashMap;
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = hashMap.find(target - nums[i]);
+            if (it != hashMap.end()) {
+                return {it->second, i};
+            }
+            hashMap[nums[i]] = i;
+            
+
+        }
+        return {};
+
+    }
+
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = sol.twoSum(nums, target);
+    for (int num : result) {
+        cout << num << endl;
+    }
+}
