a522491194ca0e6b607030c2747f3ce65fdf84c0
Marcusxixi <1654400916@qq.com>
2025-03-12 08:47:50 +0800

    add random question

---
+++ 4.随机/2000+/2566.替换一个数字后的最大差值/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <map>
+using namespace std;
+
+
+
+
+
+int main(int argc, char const *argv[]){
+    return 0;
+}
