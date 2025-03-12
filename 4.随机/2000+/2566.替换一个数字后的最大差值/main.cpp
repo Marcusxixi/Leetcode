8b1b098cb2032e52d200b268f52f6829bd3e5450
Marcusxixi <1654400916@qq.com>
2025-03-12 09:04:11 +0800

    correct question

--- 4.随机/2000+/2566.替换一个数字后的最大差值/main.cpp
+++ 4.随机/2000+/2566.替换一个数字后的最大差值/main.cpp
@@ -8,9 +8,24 @@
 using namespace std;
 
 
-
+class Solution {
+public:
+    int minMaxDifference(int num) {
+        string maxNumStr = to_string(num);
+        string minNumStr = to_string(num);
+        char maxChar = maxNumStr[0];
+        char minChar = minNumStr[0];
+        replace(maxNumStr.begin(), maxNumStr.end(), maxChar, '9');
+        replace(minNumStr.begin(), minNumStr.end(), minChar, '0');
+        cout << maxNumStr << " " << minNumStr << endl;
+        return stoi(maxNumStr) - stoi(minNumStr);
+    }
+};
 
 
 int main(int argc, char const *argv[]){
+    int num = 11891;
+    Solution sol;
+    cout << sol.minMaxDifference(num) << endl;
     return 0;
 }
