0340cdcfdd1ddb670794a9cf66172ca5bee75342
Marcusxixi <1654400916@qq.com>
2025-03-14 17:04:38 +0800

    add question 692

--- 2.数据结构/7.堆/692.前K个高频单词/main.cpp
+++ 2.数据结构/7.堆/692.前K个高频单词/main.cpp
@@ -10,15 +10,27 @@ class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
         unordered_map<string, int> hash_table;
-        for (auto & word : words){
+        for(const string& word : words) {
             hash_table[word]++;
         }
-        //定义小根堆
-        auto cmp = [](const pair<string, int>&a, const pair<string, int> & b){
-            return a.second == b.second? a.first < b.first : a.second > b.second;
-        };
-        priority
 
+        auto cmp = [](const pair<string, int>& a, const pair<string, int> b) -> bool{
+            return a.second == b.second ? a.first < b.first : a.second > b.second;
+        };
+
+        priority_queue<pair<string, int>, vector<pair<string, int>>, decltype(cmp)> pq(cmp);
+
+        for (const auto& item : hash_table) {
+            pq.emplace(item);
+            if (pq.size() > k) pq.pop();
+        }
+
+        vector<string>result(k);
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = pq.top().first;
+            pq.pop();
+        }
+        return result;
     }
 };
 
