0ecc98ed3e17e85020a101bb3feb3331fcbbac11
Marcusxixi <1654400916@qq.com>
2025-03-31 15:20:18 +0800

    add question 34

---
+++ 3.HOT100/11.二分查找/34.在排序数组仲查找元素的第一个和最后一个位置/main.cpp
@@ -0,0 +1,24 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int lower_bound(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start = lower_bound(nums, target);
+        if (start == nums.size() || nums[start] != target) {
+            return {-1, -1};
+        }
+        int end = lower_bound(nums, target + 1) - 1;
+        return {start, end};
+    }
+};
