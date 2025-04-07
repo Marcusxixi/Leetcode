95496fff9574766fcef3de9c8cdd232f7a82ba30
Marcusxixi <1654400916@qq.com>
2025-04-07 09:53:35 +0800

    add question 26

--- 2.Top Interview 150/1.Array/26.删除有序数组中的重复项/main.cpp
+++ 2.Top Interview 150/1.Array/26.删除有序数组中的重复项/main.cpp
@@ -6,7 +6,7 @@ class Solution {
         int removeDuplicates(vector<int>& nums) {
             int slow = 1;
             for (int fast = 1; fast < nums.size(); ++fast) {
-                if (nums[fast] != nums[fast - 1]) {
+                if (nums[fast] != nums[slow - 1]) {
                     nums[slow] = nums[fast];
                     ++slow;
                 }
