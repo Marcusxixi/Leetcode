02a4d9b7f039c204f89996e47e320bacfb43c8e3
Marcusxixi <1654400916@qq.com>
2025-04-07 10:14:06 +0800

    add question 189

---
+++ 2.Top Interview 150/1.Array/189.轮转数组/main.cpp
@@ -0,0 +1,14 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> arr(n);
+        for (int i = 0; i < n; ++i) {
+            arr[(i + k) % n] = nums[i];
+        }
+        swap(arr, nums);
+    }
+};
