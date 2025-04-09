9a93511af98d590d274d6b8f9b4a185698acd81c
Marcusxixi <1654400916@qq.com>
2025-04-09 14:52:26 +0800

    add question 125

---
+++ 2.面试经典150题/2.Two Pointer/125.验证回文串/main.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            while (left < right && !isalnum(s[left])) ++left;
+            while (left < right && !isalnum(s[right])) --right;
+            if (left < right) if (tolower(s[left++]) != tolower(s[right--])) return 0;
+        }
+        return 1;
+    }
+};
