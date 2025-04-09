fa5415785be8125aea51074bbdd120a63d24f171
Marcusxixi <1654400916@qq.com>
2025-04-09 17:06:11 +0800

    modify question 274

--- 2.面试经典150题/1.Array/11-20/274.H指数/main.cpp
+++ 2.面试经典150题/1.Array/11-20/274.H指数/main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int left = 0, right = citations.size() - 1;
+        int left = 0, right = citations.size();
         while (left < right) {
-            int mid = (left + right + 1) >> 1;
-            int cnt = 0;
+            int mid = (right + left + 1) >> 1;
+            int count = 0;
             for (int i = 0; i < citations.size(); ++i) {
-                if (citations[i] >= mid) ++cnt;
+                if (citations[i] >= mid) ++count;
             }
-            if (cnt >= mid) left = mid;
+            if (count >= mid) left = mid;
             else right = mid - 1;
         }
         return left;
