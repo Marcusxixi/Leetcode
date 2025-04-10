2067fc9fb990e1c093d32fd36d518b8031884efe
Marcusxixi <1654400916@qq.com>
2025-04-10 19:45:07 +0800

    add question 383

---
+++ 2.面试经典150题/5.Hashmap/383.赎金信/main.cpp
@@ -0,0 +1,17 @@
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<int, int> hash_table;
+        for (char& ch: magazine) {
+            ++hash_table[ch];
+        }
+
+        for (char& ch: ransomNote) {
+            if (!hash_table.count(ch) || --hash_table[ch] < 0) return false;
+        }
+        return true;
+    }
+};
