520126986fd2d47ffea28e5125a203080fc9bfc4
Marcusxixi <1654400916@qq.com>
2025-04-12 15:26:41 +0800

    add question 141

---
+++ 2.面试经典150题/8.Linked List/141.环形链表/main.cpp
@@ -0,0 +1,21 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = new ListNode(0);
+        slow->next = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) return true;
+        }
+        return false;
+
+    }
+};
