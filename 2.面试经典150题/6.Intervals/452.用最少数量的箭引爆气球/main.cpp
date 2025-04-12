2f74c8021ba9747f055e55a4151c50292092fa64
Marcusxixi <1654400916@qq.com>
2025-04-12 09:50:57 +0800

    add question452

---
+++ 2.面试经典150题/6.Intervals/452.用最少数量的箭引爆气球/main.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if (points.empty()) return 0;
+        // 通过 vector 中的第二个元素进行升序排序
+        sort(points.begin(), points.end(), [](const vector<int>& u, const vector<int>& v) {
+            return u[1] < v[1];
+        });
+        int pos = points[0][1];
+        int ret = 1;
+        for (const vector<int>& balloon: points) {
+            if (balloon[0] > pos) {
+                pos = balloon[1];
+                ++ret;
+            }
+        }
+        return ret;
+    }
+};
