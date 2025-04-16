d67add34640f2b154e2ac3a8b01cc8ad547c31df
Marcusxixi <1654400916@qq.com>
2025-04-16 09:38:06 +0800

    add question 236

---
+++ 2.面试经典150题/9.Binary Tree General/11-14/236.二叉树的最近公共祖先/main.cpp
@@ -0,0 +1,20 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    TreeNode* ans;
+    
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left && right) return root;
+        if (left) return left;
+        if (right) return right;
+    }
+};
