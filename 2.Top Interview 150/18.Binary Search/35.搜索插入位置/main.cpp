5686fd85e602e63dadd005951ef443d07f87c74e
Marcusxixi <1654400916@qq.com>
2025-04-19 13:59:42 +0800

    add question 35

---
+++ 2.Top Interview 150/18.Binary Search/35.搜索插入位置/main.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size();
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            return left;
+        }
+    }
+};
