e40a2ed648d1f0cff089c9b5cdada83c77d64e83
Marcusxixi <1654400916@qq.com>
2025-04-19 14:36:32 +0800

    modify q35

--- 2.Top Interview 150/18.Binary Search/35.搜索插入位置/main.cpp
+++ 2.Top Interview 150/18.Binary Search/35.搜索插入位置/main.cpp
@@ -5,14 +5,18 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int left = 0, right = nums.size();
-        while (left <= right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if (target > nums[mid]) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-            return left;
+            if (target > nums[mid]) left = mid + 1; 
+            else right = mid;    
         }
+        return left;
+        // int left = 0, right = nums.size() - 1;
+        // while (left <= right) {
+        //     int mid = left + (right - left) / 2;
+        //     if (target > nums[mid]) left = mid + 1;
+        //     else right = mid-1;
+        // }
+        // return left++;
     }
 };
