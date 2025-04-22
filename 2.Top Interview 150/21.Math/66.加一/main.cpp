b2f2957a7c3985c11b20ff294a33561941bdfa26
Marcusxixi <1654400916@qq.com>
2025-04-22 17:31:26 +0800

    add question 66

---
+++ 2.Top Interview 150/21.Math/66.加一/main.cpp
@@ -0,0 +1,16 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; --i) {
+            ++digits[i];
+            digits[i] % 10;
+            if (digits[i] != 0) return digits;
+        }
+        vector<int> ret(digits.size() + 1);
+        ret[0] = 1;
+        return ret;
+    }
+};
