8151dfa8323e0ac81c7df1e1e3cf45b3542d9369
Marcusxixi <1654400916@qq.com>
2025-04-24 20:42:17 +0800

    add question 62

---
+++ 2.Top Interview 150/23.Multidimentional DP/62.不同路径/main.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> dp(n);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+};
