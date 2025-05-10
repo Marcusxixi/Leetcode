7b14ac61b45d66fece239de690697d3e282a01e7
Marcusxixi <1654400916@qq.com>
2025-05-10 17:00:37 +0800

    add question 78

---
+++ 5.Review/1.Beginer 70/8.算法/7.深度优先/78.子集/main.cpp
@@ -0,0 +1,28 @@
+#include<vector>
+#include<functional>
+using namespace std;
+
+class Solution {
+public:
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ret;
+        vector<int> path;
+        int n = nums.size();
+
+        function<void(int)> dfs = [&] (int index) -> void {
+            if (index == n) {
+                ret.push_back(path);
+                return;
+            }
+
+            dfs(index + 1);
+
+            path.push_back(nums[index]);
+            dfs(index + 1);
+            path.pop_back();
+        };
+        dfs(0);
+        return ret;
+    }
+};
